bfc0a83bc494137b4a9d59bb8e87269c27feea35
MartinCaraballo <martin.caraballo@correo.ucu.edu.uy>
2024-12-05 20:16:35 -0300

    init.sql update

--- init.sql
+++ init.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE p4q;
 
+\c p4q
+
 -- Create Estado (State) table
 CREATE TABLE Estado (
     id_estado SERIAL PRIMARY KEY,
@@ -19,8 +21,8 @@ CREATE TABLE Rol (
 );
 
 -- Create Departamento (Department) table
-CREATE TABLE Departamento (
-    id_departamento SERIAL PRIMARY KEY,
+CREATE TABLE Etiqueta (
+    id_etiqueta SERIAL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL UNIQUE
 );
 
@@ -49,14 +51,75 @@ CREATE TABLE Registro (
     id_ticket INTEGER NOT NULL,
     id_evento INTEGER NOT NULL,
     id_usuario INTEGER NOT NULL,
-    id_departamento INTEGER NOT NULL,
     fecha TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     comentario TEXT,
     FOREIGN KEY (id_ticket) REFERENCES Ticket(id_ticket),
     FOREIGN KEY (id_evento) REFERENCES Evento(id_evento),
-    FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario),
-    FOREIGN KEY (id_departamento) REFERENCES Departamento(id_departamento)
+    FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
 );
+
+-- Create Etiquetas_Ticket (Ticket_Tags) to store the relation within ticket and tags. 
+CREATE TABLE Etiquetas_Ticket(
+    id_ticket INTEGER NOT NULL,
+    id_etiqueta INTEGER NOT NULL,
+    FOREIGN KEY (id_ticket) REFERENCES Ticket(id_ticket),
+    FOREIGN KEY (id_etiqueta) REFERENCES Etiqueta(id_etiqueta)
+);
+
+-- Create INDEX in column "nombre" in table "evento"
+CREATE INDEX ix_nombre_evento ON evento (nombre);
+
+-- FUNCTION AND TRIGGER TO EXECUTE WHEN A TAG IS ADDED TO A TICKET.
+CREATE OR REPLACE FUNCTION log_add_tag()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    INSERT INTO registro (id_ticket, id_evento, id_usuario, fecha, comentario)
+    VALUES (NEW.id_ticket,
+            (SELECT id_evento FROM evento e WHERE e.nombre = 'Cambio de etiqueta'),
+            (SELECT r.id_usuario
+                FROM registro r
+                WHERE r.id_ticket = NEW.id_ticket
+                    AND r.id_evento = (SELECT id_evento FROM evento e WHERE e.nombre = 'Asignación')
+                ORDER BY r.fecha DESC
+                LIMIT 1),
+            CURRENT_TIMESTAMP,
+            'Etiqueta ' || (SELECT et.nombre FROM etiqueta et WHERE et.id_etiqueta = NEW.id_etiqueta) || ' agregada.');
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER tag_add_trigger
+    AFTER INSERT OR UPDATE
+    ON etiquetas_ticket
+    FOR EACH ROW
+EXECUTE FUNCTION log_add_tag();
+
+-- FUNCTION AND TRIGGER TO EXECUTE WHEN A TAG IS REMOVED FROM A TICKET.
+CREATE OR REPLACE FUNCTION log_remove_tag()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    INSERT INTO registro (id_ticket, id_evento, id_usuario, fecha, comentario)
+    VALUES (OLD.id_ticket,
+            (SELECT id_evento FROM evento e WHERE e.nombre = 'Cambio de etiqueta'),
+            (SELECT r.id_usuario
+                FROM registro r
+                WHERE r.id_ticket = OLD.id_ticket
+                    AND r.id_evento = (SELECT id_evento FROM evento e WHERE e.nombre = 'Asignación')
+                ORDER BY r.fecha DESC
+                LIMIT 1),
+            CURRENT_TIMESTAMP,
+            'Etiqueta ' || (SELECT et.nombre FROM etiqueta et WHERE et.id_etiqueta = OLD.id_etiqueta) || ' eliminada.');
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER tag_remove_trigger
+    AFTER DELETE
+    ON etiquetas_ticket
+    FOR EACH ROW
+EXECUTE FUNCTION log_remove_tag();
 
 -- Insert data into Estado
 INSERT INTO Estado (nombre) VALUES 
@@ -68,7 +131,8 @@ INSERT INTO Estado (nombre) VALUES
 -- Insert data into Evento
 INSERT INTO Evento (nombre) VALUES 
 ('Creación'), 
-('Asignación'), 
+('Asignación'),
+('Cambio de etiqueta'),
 ('Actualización'), 
 ('Cierre'),
 ('Confirmación');
@@ -80,7 +144,7 @@ INSERT INTO Rol (nombre) VALUES
 ('Usuario');
 
 -- Insert data into Departamento
-INSERT INTO Departamento (nombre) VALUES 
+INSERT INTO Etiqueta (nombre) VALUES 
 ('TI'), 
 ('Recursos Humanos'), 
 ('Finanzas'), 
@@ -99,8 +163,13 @@ INSERT INTO Ticket (descripcion, prioridad, titulo, id_estado) VALUES
 ('Configuración de correo electrónico', 'Urgente', 'Setup de Email Corporativo', 3);
 
 -- Insert data into Registro
-INSERT INTO Registro (id_ticket, id_evento, id_usuario, id_departamento, comentario) VALUES 
-(1, 1, 1, 1, 'Ticket inicial creado'),
-(1, 2, 2, 1, 'Ticket asignado al soporte técnico'),
-(2, 1, 3, 2, 'Solicitud de nuevo equipo enviada'),
-(3, 1, 2, 3, 'Configuración de correo iniciada');
+INSERT INTO Registro (id_ticket, id_evento, id_usuario, comentario) VALUES 
+(1, 1, 1, 'Ticket inicial creado'),
+(1, 2, 2, 'Ticket asignado al soporte técnico'),
+(2, 1, 3, 'Solicitud de nuevo equipo enviada'),
+(3, 1, 2, 'Configuración de correo iniciada');
+
+INSERT INTO Etiquetas_Ticket (id_ticket, id_etiqueta) VALUES
+(1, 1),
+(1,2),
+(1,3);
